e5b1f4aab2ade0f55b31019a7a684c82e6f1bcb8
M0hamedAshraf19 <MohamedAshrafBedir@proton.me>
2026-07-01 12:55:18 +0300

    edits

--- Tests/He-She/script.js
+++ Tests/He-She/script.js
@@ -32,4 +32,96 @@ function setCookie(name, value, expires = null) {
     }
 }
 
-// 
+const fileNames = ["F-Airplane.jpeg",
+"F-Ball.png",
+"F-Butterfly.jpg",
+"F-Car.png",
+"F-Cat.jpg",
+"F-Clock.jpeg",
+"F-Duck.jpg",
+"F-Eagles.png",
+"F-Eyes.jpg",
+"F-Flower.jpg",
+"F-Giraffe.jpg",
+"F-Girl.png",
+"F-Keys.jpg",
+"F-Onion.jpg",
+"F-Parrots.jpg",
+"F-Pencils.jpg",
+"F-Sun.jpg",
+"F-Teeth.png",
+"F-Tree.jpg",
+"M-Alarm Clock.jpg",
+"M-Bird.jpg",
+"M-Book.jpg",
+"M-Box.jpg",
+"M-Boy.png",
+"M-Computer.jpg",
+"M-Desk.jpg",
+"M-Dog.jpg",
+"M-Eagle.png",
+"M-Elephant.jpg",
+"M-Horse.jpg",
+"M-Key.jpg",
+"M-Lion.jpg",
+"M-Monkey.jpg",
+"M-Moon.jpg",
+"M-Pencil.jpg",
+"M-Sheep.jpg",
+"M-Whale.jpg"]
+
+if (getCookie('questions') === null || getCookie('correct') === null) {
+    deleteCookies()
+    setCookie('questions', '0')
+    setCookie('correct', '0')
+} else{}
+
+if (getCookie('usedNames') === null) {
+    setCookie('usedNames',JSON.stringify([]))
+} else {
+    console.log(JSON.parse(getCookie('usedNames')))
+    if (JSON.parse(getCookie('usedNames')).length === fileNames.length) {
+        setCookie('lastUsedName',JSON.parse(getCookie('usedNames'))[fileNames.length])
+        setCookie('usedNames',JSON.stringify([]))
+    }
+}
+
+document.querySelector('#score').innerHTML = `
+    Questions: ${getCookie('questions')}<br>Correct: ${getCookie('correct')}
+`
+document.querySelector('#score').style.display = 'block'
+
+let el = ''
+
+if (getCookie('Gender') === null) {
+    el = document.querySelector("#question")
+    el.style.display = 'block'
+    let Name = ''
+    if (Math.floor(Math.random() * 2) === 0) {
+        el.innerHTML = `
+        <form method='post'>
+            <input type='submit' disabled name = '1' value='مذكر'/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type='submit' disabled name = '1' value='مونث'/><br><br>
+        </form>
+        <img src='${Name}' style='width: 50%; height: 50%;'>
+        `
+    } else {
+        el.innerHTML = `
+        <form method='post'>
+            <input type='submit' disabled name = '1' value='مونث'/>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <input type='submit' disabled name = '1' value='مذكر'/><br><br>
+        </form>
+        <img src='${Name}' style='width: 50%; height: 50%;'>
+        `
+    }
+
+    finish()
+}
+
+
+
+function finish() {
+    document.querySelectorAll("input[type='submit']").forEach(function(button) {
+        button.disabled = false
+    })
+}
